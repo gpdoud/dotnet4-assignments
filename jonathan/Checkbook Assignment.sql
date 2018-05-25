0fb7d1fa2cc97f297b36ae3ff07cbaae997a7543
Jonathan Wilson <jon.wilsond@gmail.com>
2018-05-25 10:16:37 -0400

    Added procedure to create table items

--- jonathan/Checkbook Assignment.sql
+++ jonathan/Checkbook Assignment.sql
@@ -25,6 +25,30 @@ Values
 
 go
 
+--Create procedure for populating the table
+Create procedure PopulateTable
+	@Date date,
+	@Memo nvarchar(80),
+	@Amt decimal(10,2)
+as
+begin
+	insert into Savings
+		(Date,memo,amount)
+	Values
+		(@Date,@Memo,@Amt)
+End
+
+go
+
+exec PopulateTable 	'2017-01-01', 'Initial Deposit', 500.00;
+exec PopulateTable	'2017-02-15', 'Deposit check', 250.00;
+exec PopulateTable	'2017-03-07', 'Car payment', -175.00;
+exec PopulateTable	'2017-04-11', 'Deposit birthday money', 200.00;
+exec PopulateTable	'2017-05-25', 'Pay deposit on new stereo', -50.00;
+exec PopulateTable	'2019-12-31', 'Deposit Lotto winners', 1000000.00;
+
+go
+
 --Update car payment
 update Savings
 set amount = -200 
@@ -41,3 +65,4 @@ go
 Select sum(amount) as 'Balance of account'
 from Savings
 
+select * from savings
